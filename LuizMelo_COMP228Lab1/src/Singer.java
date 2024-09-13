8a2b9074dc95966558fb2d486f6293fd1570c4e0
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-09-13 10:34:23 -0400

    Singer-class-attributes

---
+++ LuizMelo_COMP228Lab1/src/Singer.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Singer {
+
+  private int id;
+  private String name;
+  private String address;
+  private Date dateOfBirthday;
+  private int numberOfAlbumsPublished;
+}
