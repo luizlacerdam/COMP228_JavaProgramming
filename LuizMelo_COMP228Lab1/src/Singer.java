d3698701dfec71a6b02c59b4c343746fd6995826
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-09-13 10:40:18 -0400

    Singer-setters-and-getterss

--- LuizMelo_COMP228Lab1/src/Singer.java
+++ LuizMelo_COMP228Lab1/src/Singer.java
@@ -7,4 +7,52 @@ public class Singer {
   private String address;
   private Date dateOfBirthday;
   private int numberOfAlbumsPublished;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public Date getDateOfBirthday() {
+    return dateOfBirthday;
+  }
+
+  public void setDateOfBirthday(Date dateOfBirthday) {
+    this.dateOfBirthday = dateOfBirthday;
+  }
+
+  public int getNumberOfAlbumsPublished() {
+    return numberOfAlbumsPublished;
+  }
+
+  public void setNumberOfAlbumsPublished(int numberOfAlbumsPublished) {
+    this.numberOfAlbumsPublished = numberOfAlbumsPublished;
+  }
+
+  public void setAllAttributes(int id, String name, String address, Date dateOfBirthday, int numberOfAlbumsPublished) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.dateOfBirthday = dateOfBirthday;
+    this.numberOfAlbumsPublished = numberOfAlbumsPublished;
+  }
 }
