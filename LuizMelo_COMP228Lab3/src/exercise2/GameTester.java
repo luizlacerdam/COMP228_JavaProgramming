ce6113ae549a6f47ceca6d42477f5e558587d0f0
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-10-11 14:42:20 -0400

    :recycle: [refact] GameTester

--- LuizMelo_COMP228Lab3/src/exercise2/GameTester.java
+++ LuizMelo_COMP228Lab3/src/exercise2/GameTester.java
@@ -5,6 +5,9 @@ public abstract class GameTester {
   protected String name;
   protected boolean fulltime;
 
+  public GameTester(String name, boolean fulltime) {
+    this.name = name;
+    this.fulltime = fulltime;
+  }
   public abstract void salary();
-
 }
