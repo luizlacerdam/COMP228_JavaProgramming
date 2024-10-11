604cf689d83ed32465ffd7b7ba4dea4fa6faee23
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-10-11 15:07:38 -0400

    :sparkles: [feat] Main

--- LuizMelo_COMP228Lab3/src/exercise2/Main.java
+++ LuizMelo_COMP228Lab3/src/exercise2/Main.java
@@ -1,5 +1,38 @@
 package exercise2;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+  public static void main(String[] main) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the name of the game tester:");
+    String name = scanner.nextLine();
+
+    System.out.println("Choose the type of game tester:");
+    System.out.println("1. Full-Time");
+    System.out.println("2. Part-Time");
+    int choice = scanner.nextInt();
+
+    GameTester tester = null;
+
+    if (choice == 1) {
+      tester = new FullTimeGameTester(name);
+    } else if (choice == 2) {
+      System.out.println("Enter the number of hours worked:");
+      int hoursWorked = scanner.nextInt();
+      tester = new PartTimeGameTester(name, hoursWorked);
+    } else {
+      System.out.println("Invalid option.");
+      System.exit(0);
+    }
+
+    System.out.println("\nGame Tester Information:");
+    System.out.println("Name: " + tester.name);
+    System.out.println("Employment Type: " + (tester.fulltime ? "Full-Time" : "Part-Time"));
+    System.out.println("Salary: $" + tester.salary());
+
+
+    scanner.close();
+  }
 }
