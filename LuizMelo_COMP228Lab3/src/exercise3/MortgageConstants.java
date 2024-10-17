08b76168e1592e9d3f0c9dc42fed80603170bad7
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-10-17 10:30:24 -0400

    MortgageConstantes

---
+++ LuizMelo_COMP228Lab3/src/exercise3/MortgageConstants.java
@@ -0,0 +1,10 @@
+package exercise3;
+
+public interface MortgageConstants {
+  int SHORT_TERM = 1;
+  int MEDIUM_TERM = 3;
+  int LONG_TERM = 5;
+  String BANK_NAME = "City Toronto Bank";
+  double MAX_MORTGAGE_AMOUNT = 300000;
+
+}
