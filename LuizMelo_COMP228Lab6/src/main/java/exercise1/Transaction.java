dc0618b930a9e91a7ac97ab99f8579c8eabeb770
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-12-10 11:18:59 -0500

    :sparkles: feat: Transaction class

---
+++ LuizMelo_COMP228Lab6/src/main/java/exercise1/Transaction.java
@@ -0,0 +1,25 @@
+package exercise1;
+
+public class Transaction implements Runnable {
+  private final Account account;
+  private final double amount;
+  private final String transactionType;
+
+  public Transaction(Account account, double amount, String transactionType) {
+    this.account = account;
+    this.amount = amount;
+    this.transactionType = transactionType;
+  }
+
+  @Override
+  public void run() {
+    if ("deposit".equalsIgnoreCase(transactionType)) {
+      account.deposit(amount);
+    } else if ("withdraw".equalsIgnoreCase(transactionType)) {
+      account.withdraw(amount);
+    } else {
+      System.out.println("Invalid transaction type: " + transactionType);
+    }
+  }
+}
+
