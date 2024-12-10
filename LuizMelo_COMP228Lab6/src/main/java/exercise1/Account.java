8ed6c9b75fd411549efb3f7dc8f882d6514b9e5c
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-12-10 11:14:35 -0500

    :sparkles: feat: Account class

---
+++ LuizMelo_COMP228Lab6/src/main/java/exercise1/Account.java
@@ -0,0 +1,31 @@
+package exercise1;
+
+public class Account {
+  private double balance;
+
+  public Account(double initialBalance) {
+    if (initialBalance > 0.0) {
+      this.balance = initialBalance;
+    }
+  }
+
+  public synchronized void deposit(double amount) {
+    if (amount > 0.0) {
+      balance += amount;
+      System.out.printf("Deposited: %.2f, New Balance: %.2f%n", amount, balance);
+    }
+  }
+
+  public synchronized void withdraw(double amount) {
+    if (amount > 0.0 && amount <= balance) {
+      balance -= amount;
+      System.out.printf("Withdrawn: %.2f, New Balance: %.2f%n", amount, balance);
+    } else {
+      System.out.printf("Withdrawal of %.2f failed. Insufficient funds. Balance: %.2f%n", amount, balance);
+    }
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+}
